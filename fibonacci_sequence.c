aca41f513f80ee43788ae29291e794afc57171ef
idahamondi <159940437+idahamondi@users.noreply.github.com>
2024-02-16 23:29:49 +0300

    Fibonacci sequence

---
+++ fibonacci_sequence.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void generateFibonacci(int n) {
+    int first = 0, second = 1, next;
+
+    printf("Fibonacci Series up to %d terms:\n", n);
+
+    for (int i = 0; i < n; i++) {
+        if (i <= 1)
+            next = i;
+        else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d, ", next);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of terms for Fibonacci Series: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Invalid input! Please enter a positive number.\n");
+    } else {
+        generateFibonacci(n);
+    }
+
+    return 0;
+}
